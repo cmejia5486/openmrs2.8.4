097f1c784fcb3ea27d7cb127690f88426876c570
cmejia5486 <52473133+cmejia5486@users.noreply.github.com>
2026-02-07 13:33:32 -0500

    update

---
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/patientdashboard/charts/PatientChartsFragment.java
@@ -0,0 +1,209 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.mobile.activities.patientdashboard.charts;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.common.collect.Lists;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.openmrs.mobile.R;
+import org.openmrs.mobile.activities.patientdashboard.PatientDashboardActivity;
+import org.openmrs.mobile.activities.patientdashboard.PatientDashboardContract;
+import org.openmrs.mobile.activities.patientdashboard.PatientDashboardFragment;
+import org.openmrs.mobile.models.Encounter;
+import org.openmrs.mobile.models.Observation;
+import org.openmrs.mobile.models.Visit;
+import org.openmrs.mobile.utilities.ApplicationConstants;
+import org.openmrs.mobile.utilities.ToastUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+
+public class PatientChartsFragment extends PatientDashboardFragment implements PatientDashboardContract.ViewPatientCharts {
+    private ListView mListView;
+    private TextView mEmptyListView;
+    private JSONObject observationList;
+    private PatientChartsListAdapter chartsListAdapter;
+    private PatientDashboardActivity mPatientDashboardActivity;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setPresenter(mPresenter);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_patient_charts, null, false);
+
+        mEmptyListView = root.findViewById(R.id.vitalEmpty);
+        mListView = root.findViewById(R.id.vitalList);
+        mListView.setEmptyView(mEmptyListView);
+        setEmptyListVisibility(false);
+
+        mListView.setOnItemClickListener((parent, view, position, id) -> {
+
+            String vitalName = chartsListAdapter.getItem(position);
+            try {
+                JSONObject chartData = observationList.getJSONObject(vitalName);
+                Iterator<String> dates = chartData.keys();
+                ArrayList<String> dateList = Lists.newArrayList(dates);
+
+                if(dateList.size() == 0)
+                    ToastUtil.showShortToast(getContext(), ToastUtil.ToastType.ERROR, getString(R.string.data_not_available_for_this_field));
+                else {
+                    JSONArray dataArray = chartData.getJSONArray(dateList.get(0));
+                    String entry = (String) dataArray.get(0);
+                    try {
+                        Float entryValue = Float.parseFloat(entry);
+                        Intent intent = new Intent(getActivity(), ChartsViewActivity.class);
+                        Bundle mBundle = new Bundle();
+                        mBundle.putString("vitalName", chartData.toString());
+                        intent.putExtra("bundle", mBundle);
+                        startActivity(intent);
+                    } catch (NumberFormatException e) {
+                        ToastUtil.showShortToast(getContext(), ToastUtil.ToastType.ERROR, getString(R.string.data_type_not_available_for_this_field));
+                    }
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+                ToastUtil.showShortToast(getContext(), ToastUtil.ToastType.ERROR, e.getMessage());
+            }
+
+        });
+        return root;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // This method is intentionally empty
+    }
+
+    public static PatientChartsFragment newInstance() {
+        return new PatientChartsFragment();
+    }
+
+    @Override
+    public void setEmptyListVisibility(boolean visibility) {
+        if (visibility) {
+            mEmptyListView.setVisibility(View.VISIBLE);
+        } else {
+            mEmptyListView.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void populateList(List<Visit> visits) {
+        final String[] displayableEncounterTypes = ApplicationConstants.EncounterTypes.ENCOUNTER_TYPES_DISPLAYS;
+        final HashSet<String> displayableEncounterTypesArray = new HashSet<>(Arrays.asList(displayableEncounterTypes));
+        observationList = new JSONObject();
+
+        for (Visit visit : visits) {
+            List<Encounter> encounters = visit.getEncounters();
+            if (!encounters.isEmpty()) {
+                setEmptyListVisibility(false);
+                for (Encounter encounter : encounters) {
+                    String datetime = encounter.getEncounterDate();
+                    String encounterTypeDisplay = encounter.getEncounterType().getDisplay();
+                    if (displayableEncounterTypesArray.contains(encounterTypeDisplay)) {
+                        for (Observation obs : encounter.getObservations()) {
+                            String observationLabel = obs.getDisplay();
+                            if (observationLabel.contains(":")) {
+                                observationLabel = observationLabel.substring(0, observationLabel.indexOf(':'));
+                            }
+                            if (observationList.has(observationLabel)) {
+                                JSONObject chartData = null;
+                                try {
+                                    chartData = observationList.getJSONObject(observationLabel);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                if (chartData.has(datetime)) {
+                                    JSONArray obsValue = null;
+                                    try {
+                                        obsValue = chartData.getJSONArray(datetime);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                    obsValue.put(obs.getDisplayValue());
+                                    try {
+                                        chartData.put(datetime, obsValue);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                } else {
+                                    JSONArray obsValue = new JSONArray();
+                                    obsValue.put(obs.getDisplayValue());
+                                    try {
+                                        chartData.put(datetime, obsValue);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            } else {
+                                JSONObject chartData = new JSONObject();
+                                JSONArray obsValue = new JSONArray();
+                                obsValue.put(obs.getDisplayValue());
+                                try {
+                                    chartData.put(datetime, obsValue);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                try {
+                                    observationList.put(observationLabel, chartData);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            chartsListAdapter = new PatientChartsListAdapter(this.getActivity(), observationList);
+            mListView.setAdapter(chartsListAdapter);
+        }
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser) {
+            try {
+                mPatientDashboardActivity.hideFABs(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/providerdashboard/ProviderDashboardPresenter.java
@@ -0,0 +1,89 @@
+package org.openmrs.mobile.activities.providerdashboard;
+
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+
+import org.openmrs.mobile.activities.BasePresenter;
+import org.openmrs.mobile.api.CustomApiCallback;
+import org.openmrs.mobile.api.RestApi;
+import org.openmrs.mobile.api.RestServiceBuilder;
+import org.openmrs.mobile.api.repository.ProviderRepository;
+import org.openmrs.mobile.models.Provider;
+import org.openmrs.mobile.utilities.ApplicationConstants;
+
+public class ProviderDashboardPresenter extends BasePresenter implements ProviderDashboardContract.Presenter, CustomApiCallback {
+    private RestApi restApi;
+    private ProviderRepository providerRepository;
+    private Provider provider;
+    @NonNull
+    private ProviderDashboardContract.View providerDashboardView;
+
+    public ProviderDashboardPresenter(@NonNull ProviderDashboardContract.View view, Context context) {
+        this.providerDashboardView = view;
+        this.providerDashboardView.setPresenter(this);
+        this.restApi = RestServiceBuilder.createService(RestApi.class);
+        providerRepository = new ProviderRepository(context);
+    }
+
+    public ProviderDashboardPresenter(@NonNull ProviderDashboardContract.View view, RestApi restApi, ProviderRepository providerRepository) {
+        this.providerDashboardView = view;
+        this.providerDashboardView.setPresenter(this);
+        this.restApi = restApi;
+        this.providerRepository = providerRepository;
+    }
+
+    @Override
+    public Provider getProviderFromIntent(Intent intent) {
+        return (Provider) (intent.getSerializableExtra(ApplicationConstants.BundleKeys.PROVIDER_ID_BUNDLE));
+    }
+
+    @Override
+    public void updateProvider(Provider provider) {
+        providerRepository.updateProvider(restApi, provider, new CustomApiCallback() {
+            @Override
+            public void onSuccess() {
+                providerDashboardView.setupBackdrop(provider);
+            }
+
+            @Override
+            public void onFailure() {
+                providerDashboardView.showSnackbarForFailedEditRequest();
+            }
+        });
+    }
+
+    @Override
+    public void deleteProvider() {
+        providerRepository.deleteProviders(restApi, provider.getUuid(), this);
+    }
+
+    @Override
+    public void subscribe() {
+
+    }
+
+    @Override
+    public void onFailure() {
+
+    }
+
+    /**
+     * no need of refreshUI in this onSuccess() callback
+     * since onUpdate() will rebuild this dashboard again
+     * and onDelete this dashboard will be closed as activity finishes
+     */
+    @Override
+    public void onSuccess() {
+
+    }
+
+    public Provider getProvider() {
+        return provider;
+    }
+
+    public void setProvider(Provider provider) {
+        this.provider = provider;
+    }
+}
